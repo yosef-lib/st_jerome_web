88cc804d348653ab2944415e7b2cd5518bd8da65
tutonutsurabaya-alt <yosefpaskah@gmail.com>
2026-09-18 13:23:16 +0700

    fix: make start.sh resilient to venv/gunicorn

--- start.sh
+++ start.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-gunicorn app:app -b 127.0.0.1:5001
+if [ -d "venv" ]; then
+    source venv/bin/activate
+elif [ -d "env" ]; then
+    source env/bin/activate
+fi
+
+if command -v gunicorn &> /dev/null; then
+    gunicorn app:app -b 127.0.0.1:5001
+else
+    python3 -m flask run -h 127.0.0.1 -p 5001
+fi
