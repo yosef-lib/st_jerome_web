de30a52862bb09d464e9ce8030af974a889066fa
tutonutsurabaya-alt <yosefpaskah@gmail.com>
2026-09-24 12:26:32 +0700

    fix: add make_response to flask imports

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify, send_file, session, flash
+from flask import Flask, render_template, request, redirect, url_for, jsonify, send_file, session, flash, make_response
 import csv
 import database
 import json
